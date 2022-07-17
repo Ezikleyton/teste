5eb64ed8e48c4e78b94d253a757139c1a58f63f3
ezikleyton <ezikleyton@gmail.com>
2022-07-17 13:41:53 -0300

    adição da função de adição no testes.py

--- testes.py
+++ testes.py
@@ -1,1 +1,5 @@
 print ("Hello World");
+numero1 = float (input("Insira um numero: "))
+numero2 = float (input("Insira outro numero: "))
+soma = numero1 + numero2
+print ("A soma de {:.0f} + numero {:.0f} é igual a {:.0f}".format(numero1,numero2,soma))
